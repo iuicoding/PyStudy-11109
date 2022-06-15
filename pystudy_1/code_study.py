ba84c1a0cc631de17b18958743f48c2657f27533
alstn8022 <niceminsoo06@gmail.com>
2022-06-15 15:50:16 +0900

    1차시 수업_최종의 최종

--- pystudy_1/code_study.py
+++ pystudy_1/code_study.py
@@ -4,9 +4,13 @@ print('Hello, World!')
 print("Hello, World!")
 # Hello, World!를 출력
 
+# 좀 짧은 주석
+
 """
 아주
+
 긴
+
 주석
 """
 
@@ -17,17 +21,18 @@ print(24, 55)
 # 24 55를 출력
 
 print("'안녕하세요'라고 말했습니다")
+print('"안녕하세요"라고 말했습니다.')
 
 print("\"안녕하세요\"라고 말했습니다.")
 
 print("안녕"+"하세요")
 print("안녕하세요"*3)
 print("3//2=", 3//2)
-print(2*3)
-print(343-32)
-print(90/3)
+print("2*3=", 2*3)
+print("343-32=" ,343-32)
+print("90/3=" ,90/3)
 print("5%2=", 5%2)
-print(2**1)
+print("2**1=", 2**1)
 
 string = input("인사말을 입력하세요")
 print(string)
