6831083e4ddf3d056769e2baee497200e49f8bc2
alstn8022 <niceminsoo06@gmail.com>
2022-06-15 15:42:37 +0900

    1st hour class

--- pystudy_1/code_study.py
+++ pystudy_1/code_study.py
@@ -1,1 +1,33 @@
-print('hello world!')
+print('Hello, World!')
+# Hello, World!를 출력
+
+print("Hello, World!")
+# Hello, World!를 출력
+
+"""
+아주
+긴
+주석
+"""
+
+print(1+34)
+# 35를 출력
+
+print(24, 55)
+# 24 55를 출력
+
+print("'안녕하세요'라고 말했습니다")
+
+print("\"안녕하세요\"라고 말했습니다.")
+
+print("안녕"+"하세요")
+print("안녕하세요"*3)
+print("3//2=", 3//2)
+print(2*3)
+print(343-32)
+print(90/3)
+print("5%2=", 5%2)
+print("2**1")
+
+string=input("인사말을 입력하세요")
+print(string)
