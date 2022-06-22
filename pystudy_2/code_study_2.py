a481e0db87cb8f24c4eea6ed26ef8612ea397c2e
alstn8022 <niceminsoo06@gmail.com>
2022-06-22 15:27:52 +0900

    2차시 수업

--- pystudy_2/code_study_2.py
+++ pystudy_2/code_study_2.py
@@ -1,19 +1,27 @@
-score = float(input("당신의 점수를 입력해주세요."))
+list_a = ['인터넷', '익스플로러', '죽음']
+print(list_a[0])
 
-if 9 < score and score <= 10:
-    print("S+ Rank")
-elif 8 < score:
-    print("S Rank")
-elif 7 < score:
-    print("A Rank")
-elif 6 < score:
-    print("B Rank")
-elif 5 < score:
-    print("C Rank")
-elif 4 < score:
-    print("D Rank")
-elif 3 < score:
-    print("E Rank")
+list_b = ["파이참"]
+print(list_b[0])
+
+list_1 = [1, 2, 3]
+print(list_1 + [4])
+
+list_2 = [1, 2, 3, 4]
+list_2.insert(1, 3)
+list_2.extend([5, 6, 7, 8])
+list_2.remove(8)
+list_2.pop(3)
+
+list_3 = ["W", "a", "!", "S", "a", "n", "s"]
+for element in list_3:
+    print(element)
+
+if 9 in list_2:
+    print("값을 찾았습니다.")
 else:
-    print("F Rank")
+    print("값이 존재하지 않습니다.")
 
+str_list = ["W", "a", "!", "S", "a", "n", "s"]
+del str_list[2]
+print(str_list)
